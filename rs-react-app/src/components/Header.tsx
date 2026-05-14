b92fc586be582902938e8fc0a56cd92daafec55a
solarsungai <solarsungai@gmail.com>
2026-05-14 10:35:12 +0300

    refactor: migrate Header to functional component

--- rs-react-app/src/components/Header.tsx
+++ rs-react-app/src/components/Header.tsx
@@ -1,16 +1,12 @@
-import { Component } from 'react';
-
-class Header extends Component {
-  render() {
-    return (
-      <header className="header">
-        <h1>Pokédex Explorer</h1>
-        <p className="header-subtitle">
-          Search Pokémon and view key stats instantly.
-        </p>
-      </header>
-    );
-  }
+function Header() {
+  return (
+    <header className="header">
+      <h1>Pokédex Explorer</h1>
+      <p className="header-subtitle">
+        Search Pokémon and view key stats instantly.
+      </p>
+    </header>
+  );
 }
 
 export default Header;
