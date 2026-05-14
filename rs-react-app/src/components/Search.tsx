e227973f494db918ce5949b797ca58a9f8eb19a4
solarsungai <solarsungai@gmail.com>
2026-05-14 10:41:01 +0300

    refactor: migrate Search to functional component

--- rs-react-app/src/components/Search.tsx
+++ rs-react-app/src/components/Search.tsx
@@ -1,27 +1,20 @@
-import { Component } from 'react';
 import type { SearchProps } from '../types';
 
-class Search extends Component<SearchProps> {
-  constructor(props: SearchProps) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="search-controls">
-        <input
-          className="search-input"
-          type="text"
-          placeholder="Search..."
-          value={this.props.value}
-          onChange={(e) => this.props.onChange(e.target.value)}
-        />
-        <button className="search-button" onClick={this.props.onSearch}>
-          Search
-        </button>
-      </div>
-    );
-  }
+function Search({ value, onChange, onSearch }: SearchProps) {
+  return (
+    <div className="search-controls">
+      <input
+        className="search-input"
+        type="text"
+        placeholder="Search..."
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      />
+      <button className="search-button" onClick={onSearch}>
+        Search
+      </button>
+    </div>
+  );
 }
 
 export default Search;
