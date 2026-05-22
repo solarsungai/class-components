254537812394e8131078690911780dc8ba0cd94f
solarsungai <solarsungai@gmail.com>
2026-05-22 15:56:08 +0300

    feat: implement csv file download

--- rs-react-app/src/components/Flyout.tsx
+++ rs-react-app/src/components/Flyout.tsx
@@ -2,12 +2,16 @@ import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { clearAllSelections } from '../store/pokemonSlice';
 import type { RootState } from '../store';
+import type { PokemonData } from '../types';
+import { SERVER_URL } from '../constants';
+import { fetchPokemonByTerm } from '../services/api';
 
 function Flyout() {
   const selectedPokemons = useSelector(
     (state: RootState) => state.pokemon.selectedNames
   );
   const dispatch = useDispatch();
+  const [downloadError, setDownloadError] = useState<string | null>(null);
   const hasItems = selectedPokemons.length > 0;
   const [shouldRender, setShouldRender] = useState(hasItems);
 
@@ -24,16 +28,44 @@ function Flyout() {
 
   const handleUnselectAll = () => {
     dispatch(clearAllSelections());
+    setDownloadError(null);
   };
 
-  const handleDownload = () => {
-    console.log(`Downloading CSV for: ${selectedPokemons.join(', ')}`);
+  const handleDownload = async () => {
+    try {
+    setDownloadError(null);
+    const results: PokemonData[] = await Promise.all(selectedPokemons.map(pokemon => fetchPokemonByTerm(SERVER_URL, pokemon)));
+    let csvContent = 'Name,Types,Height (m),Weight (kg),Base XP,Details URL\n';
+    results.forEach((pokemon) => {
+      const pokemonTypes = pokemon.types ? pokemon.types.join(', ') : 'none';
+      const heightM = pokemon.height ? pokemon.height / 10 : 0;
+      const weightKg = pokemon.weight ? pokemon.weight / 10 : 0;
+      const detailsUrl = `${window.location.origin}/?page=1&search=${pokemon.name}`;
+      csvContent += `"${pokemon.name}","${pokemonTypes}",${heightM},${weightKg},${pokemon.baseExperience},"${detailsUrl}"\n`;
+    });
+    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', `selected__pokemons_${results.length}.csv`);
+    link.click();
+    URL.revokeObjectURL(url);
+    } catch (error) {
+    const errorMessage = error instanceof Error ? error.message : 'Unknown error';
+    setDownloadError(`(Download failed: ${errorMessage})`);
+    }
   };
 
   return (
     <div className={`flyout-box${!hasItems ? ' flyout-box--hiding' : ''}`}>
       <div className="flyout-header">
-        <h3>Selected Pokémon ({selectedPokemons.length})</h3>
+        <h3>Selected Pokémon ({selectedPokemons.length})
+          {downloadError && (
+            <span className="flyout-error">
+              {downloadError}
+            </span>
+          )}
+        </h3>
         <div className="flyout-buttons">
           <button
             type="button"
