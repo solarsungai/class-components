567fb8a4365433399f2d3efed1ee99822045df23
solarsungai <solarsungai@gmail.com>
2026-06-19 22:37:05 +0300

    chore: migrate from Vite to Next.js, update configs and project structure, configure line endings to LF

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
 import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
 import reactPlugin from 'eslint-plugin-react';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
 import tseslint from 'typescript-eslint';
@@ -15,7 +14,6 @@ export default defineConfig([
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
       reactPlugin.configs.flat.recommended,
       reactPlugin.configs.flat['jsx-runtime'],
       eslintConfigPrettier,
